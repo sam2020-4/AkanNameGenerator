2f86982621eb472c1ff01cccf280b2180faa2707
matata samuel <mattasamuel3@gmail.com>
2020-07-18 17:00:27 +0300

    added js function on js file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// all index.js files
+function getAkanName() {
+    /* declaration  and initialisation */
+    var day = document.getElementById("day").value;
+    var dd = parseInt(day);
+    var month = document.getElementById("month").value;
+    var mm = parseInt(month);
+    var year = document.getElementById("year").value;
+    var yy = parseInt(year.toString().slice(2, 4));
+
+
+
+
+
+
+
+
+
+
+}  
