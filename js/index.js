7be2ced06533d5771bca42f3a6b75589db5b0e6f
matata samuel <mattasamuel3@gmail.com>
2020-07-18 17:26:05 +0300

    declared and initialized arrays

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
 // all index.js files
+
 function getAkanName() {
     /* the formula */
-  var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
-
-
-
-    /* array declaraction and initialization*/ 
-
-
-
-
-
+    var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
 
     /* declaration  and initialisation of date of birth */
     var day = document.getElementById("day").value;
@@ -23,7 +15,12 @@ function getAkanName() {
     /* to get the century*/
     var cc = Math.ceil(year / 100)
 
-
+    /* array declaraction and initialization*/ 
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    
+    // comparing the gender
+    
 
 
 
