5e5df4eca464e8c8117b2720bfada0095eaa2530
matata samuel <mattasamuel3@gmail.com>
2020-07-18 17:16:29 +0300

    added fourmula on js file

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 // all index.js files
 function getAkanName() {
-    /* declaration  and initialisation */
+    /* the formula */
+  var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+
+
+    /* array declaraction and initialization*/ 
+
+
+
+
+
+
+    /* declaration  and initialisation of date of birth */
     var day = document.getElementById("day").value;
     var dd = parseInt(day);
     var month = document.getElementById("month").value;
     var mm = parseInt(month);
     var year = document.getElementById("year").value;
     var yy = parseInt(year.toString().slice(2, 4));
+
+    /* to get the century*/
+    var cc = Math.ceil(year / 100)
 
 
 
